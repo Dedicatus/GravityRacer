23853e8d74e414099b61eccbe1c0227d75d1c70d
Sugina <me865@ME865deMacBook-Pro.local>
2016-12-18 21:50:49 -0800

    changed input

--- Assets/Scripts/System/InputHandler.cs
+++ Assets/Scripts/System/InputHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 
 public class InputHandler : MonoBehaviour {
+	bool flag = false;
     
 	void Awake () {
 	
@@ -11,17 +12,52 @@ public class InputHandler : MonoBehaviour {
     void Start()
     {
     }
+	void AccelarePlayer(){
+		Player.current.GetComponentInChildren<SelfRotateByZ>().rotateRate = 120;
+		Player.current.pushForce = 350;
+	}
+	void RecoverPlayer(){
+		Player.current.GetComponentInChildren<SelfRotateByZ> ().rotateRate = 60;
+		Player.current.pushForce = 150;
+	}
 
 	// Update is called once per frame
 	void Update () {
+		
         if (Player.current == null) return;
-        if (Input.GetKey(KeyCode.A))
-        {
-            Player.current.RotateLeft();
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            Player.current.RotateRight();
-        }
+
+		if (Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.D)) {
+			AccelarePlayer ();
+			flag = true;
+		} else if (Input.GetKey (KeyCode.A)) {
+			Player.current.RotateLeft ();
+		} else if (Input.GetKey (KeyCode.D)) {
+			Player.current.RotateRight ();
+		} 
+
+
+		if (Input.touchCount == 1) {
+			Touch touch = Input.touches [0];
+			if (touch.position.x < Screen.width / 2.0f) {
+				Player.current.RotateLeft ();
+			} else {
+				Player.current.RotateRight();
+			}
+		}
+		if (Input.touchCount == 2) {
+			Touch touch1 = Input.touches [0];
+			Touch touch2 = Input.touches [1];
+			if (touch1.position.x < Screen.width / 2.0f && touch2.position.x > Screen.width / 2.0f) {
+				AccelarePlayer ();
+				flag = true;
+
+			}
+
+		}
+		if (flag != true) {
+			RecoverPlayer ();
+
+		}
+		flag = false;
     }
 }
