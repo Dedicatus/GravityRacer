29bc348a14a568594c89d308c6546480a60a8d21
Handa Zhang <hzhan100@syr.edu>
2016-12-31 15:11:33 +0800

    added ui manager

--- Assets/Scripts/LevelBuilding/CoinGenerator.cs
+++ Assets/Scripts/LevelBuilding/CoinGenerator.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour {
 
+    public static CoinGenerator current;
+
+    public int coinCount;
+    public GameObject coinPrefab;
+
+    public Coin[] coins;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        current = this;
+        coins = new Coin[coinCount];
+        for (int a = 0; a != coinCount; ++a)
+        {
+            GameObject coinObject = (GameObject)Instantiate<GameObject>(coinPrefab);
+            coinObject.transform.position = Vector3.zero;
+            coins[a] = coinObject.GetComponent<Coin>();
+            coinObject.transform.parent = transform;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
